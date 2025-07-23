e3b378156e46043d1e70a47de1b75f5b61dcc0bd
Kosta Koktashev <koktashev33@gmail.com>
2025-07-23 19:59:22 +0300

    Preparing the infrarastructure for database,seeding,and general connectivity across services mostly withing the aspire apphost, ngrok commented out atm since no use for it but might come in handly later on.

--- AspireJavaScript.AppHost/AppHost.cs
+++ AspireJavaScript.AppHost/AppHost.cs
@@ -1,14 +1,34 @@
+using k8s.Models;
+
 var builder = DistributedApplication.CreateBuilder(args);
+var postgres = builder.AddPostgres("postgres").WithPgAdmin();
+var postgresVolume = postgres.WithDataVolume(isReadOnly: false);
+var ProjectAthenaDB = postgres.AddDatabase("ProjectAthena");
+var ngrokAuthToken = builder.AddParameter("ngrok-auth-token", "2zg42YIX4zF8cSEjmsq70MuxIzj_6FKWxijKnH7ZeGL5rUK6t", secret: true);
+var dbService = builder.AddProject<Projects.ProjectAthena_DbWorkerService>("ProjectAthena-DbWorkerService").WithReference(ProjectAthenaDB).WaitFor(ProjectAthenaDB);
+
 
 var weatherApi = builder.AddProject<Projects.ProjectAthena_MinimalApi>("weatherapi")
-    .WithExternalHttpEndpoints();
+    .WithExternalHttpEndpoints()
+    .WaitFor(dbService)
+    .WithReference(ProjectAthenaDB);
 
 
-builder.AddNpmApp("reactvite", "../AspireJavaScript.Vite")
+
+
+var reactClient = builder.AddNpmApp("reactvite", "../AspireJavaScript.Vite")
     .WithReference(weatherApi)
     .WithEnvironment("BROWSER", "none")
     .WithHttpEndpoint(env: "VITE_PORT")
     .WithExternalHttpEndpoints()
     .PublishAsDockerFile();
 
+
+
+//builder.AddNgrok("ngrok")
+//    .WithAuthToken(ngrokAuthToken)
+//    // Add tunnel for API service (will get a random ngrok URL)
+//    .WithTunnelEndpoint(weatherApi, "https", "smartfixpro.ngrok-free.app")
+//    // Add tunnel for Client service (will get a separate random ngrok URL)  
+//    .WithTunnelEndpoint(reactClient, "https", "smartfixproclient.ngrok-free.app");
 builder.Build().Run();
